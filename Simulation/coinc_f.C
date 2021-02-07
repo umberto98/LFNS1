63abaaedc16c332d6883ef0c831f750b850ac734
umberto98 <umbertofollo@yahoo.it>
2021-02-07 15:14:06 +0100

    aumentato la precisione
    da sistemare l'errore costante aggiunto

--- Simulation/coinc_f.C
+++ Simulation/coinc_f.C
@@ -9,7 +9,7 @@
 void coinc_f(){
 
   TH1F* graph = new TH1F("graph","Simulazione curva di coincidenza",141,-90.5,50.5);
-  TH2F* chi = new TH2F("chi","Distribuzione chi",44,4.75,10.25,36,4.75,9.25);	
+  TH2F* chi = new TH2F("chi","Distribuzione chi",176,3.75,9.25,144,6.75,11.25);	
 
   //data
   float x[29] = {0, 5, 10, 15, 20, 25, 30, 35, 37, 38, 39, 40, 42, 46, -5, -10, -15, -20, -25, -30, -35, -40, -45, -50, -52, -54, -56, -58,-60};
@@ -22,10 +22,12 @@ void coinc_f(){
   float y_err[29];
   float rate_m  = 0;  //mi serve per l'altezza del grafico
   float counter = 0;
+  float att = 2.;
+  float err_cost = 0.04;
   
   for(int i=0;i<29;i++){
     y[i] = counts[i]/time[i] - rate_bk;
-    y_err[i] = TMath::Sqrt(counts[i])/time[i];
+    y_err[i] = TMath::Sqrt(counts[i]/(time[i]*time[i]) + rate_bk/70592. + err_cost*err_cost);
     if(-30<x[i] & x[i]<26){
       rate_m+=y[i];
       counter++;
@@ -34,27 +36,27 @@ void coinc_f(){
   rate_m = rate_m/counter;
 
   //simulzione
-  double gS_width = 30.;   //è centrato in zero
-  double g1_width = 30.;
-  double rate = 1./(2*1.0197*352.81);
+  double gS_width = 30;   //è centrato in zero
+  double g1_width = 30;
+  double rate = 1./(3*1.0197*352.81);
   double scarti_min = 10000;
   double scarti_l = 0;
   double scarti_h = 0;
 
   //Calcoliamo il chi quadro per ogni simulazione
-  for(double h = 5.; h<10.; h+=0.125){
-    for(double l = 5.; l<9.; l+=0.125){
+  for(double h = 4.; h<9.; h+=0.03125){
+    for(double l = 7.; l<11.; l+=0.03125){
       double g1_sigma = h;
       double adj = l;
       graph->Reset();
   
       //mi serve per l'altezza dell'histo
       for(int i=0; i<29; i++){
-	for(int j=0; j<1800; j++){
+	for(int j=0; j<2700; j++){
 	  double temp = gRandom->Gaus(x[i],g1_sigma);
 	  if(temp<0 & temp+g1_width>=-gS_width+adj)
 	    graph->Fill(x[i]);
-	  if(temp>=0 & temp<=gS_width+adj)
+	  if(temp>=0 & temp + att <=gS_width+adj)
 	    graph->Fill(x[i]);
 	}
       }
